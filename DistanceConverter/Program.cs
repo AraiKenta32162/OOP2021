78466b8d50d028656bf1f05ac92e9ebd90e56f43
新井健太 <32162@ojs.ac.jp>
2021-05-25 10:25:09 +0900

    P52リスト2.6 変換処理をクラスに分離

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -17,26 +17,20 @@ namespace DistanceConverter {
         }
         //フィートからメートルへの対応表を出力
         private static void PrintFeetToMeterList(int start, int stop) {
-            for (int feet = start; feet < stop; feet++) {
-                double meter = FeetToMeter(feet);
+            FeetConverter converter = new FeetConverter();
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = converter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         }
         
         //メートルからフィートへの対応表を出力
         private static void PrintMeterToFeetList(int start,int stop) {
+            FeetConverter converter = new FeetConverter();
             for (int meter = start; meter < stop; meter++) {
-                double feet = MeterToFeet(meter);
-                Console.WriteLine("{0} ft = {1:0.0000} m", meter, feet);
+                double feet = converter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
-        }
-        //フィートからメートルを求める
-        private static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-        //メートルからフィートを求める
-        private static double MeterToFeet(int meter) {
-            return meter / 0.3048;
         }
     }
 }
