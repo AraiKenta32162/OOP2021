6d23ccc0a99a2f66a33f442891a3ebece91234e7
新井健太 <32162@ojs.ac.jp>
2021-05-28 16:00:04 +0900

    問題2.1終了

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace Exercise1 {
     class Program {
         static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[] {
+                new Song("a", "s", 153),
+                new Song("d", "f", 205),
+                new Song("g", "h", 317),
+            };
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine(@"{0},{1},{2:m\:ss}", song.Title, song.ArtistName,TimeSpan.FromSeconds( song.Length));
+            }
         }
     }
 }
