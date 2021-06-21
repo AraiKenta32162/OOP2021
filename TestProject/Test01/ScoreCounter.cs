c6b12a54351c444324a5efba779ac94fc9df3625
新井健太 <32162@ojs.ac.jp>
2021-06-21 14:30:51 +0900

    完成

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -7,7 +7,7 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
+            _score = ReadScore(filePath);
 
 
             
@@ -16,8 +16,18 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
+            var scores = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                var score = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                scores.Add(score);
+            }
+            return scores;
 
 
 
@@ -28,12 +38,14 @@ namespace Test01 {
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            var dict = new Dictionary<string,int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject))
+                    dict[score.Subject] += score.Score;
+                else
+                        dict[score.Subject] = score.Score;
+                }
+            return dict;
         }
     }
 }
