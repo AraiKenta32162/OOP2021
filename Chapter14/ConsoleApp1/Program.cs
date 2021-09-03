4be044841e6d6ffaf615e01e03eb2044fe877c90
健太 新井 <32162@ojs.ac.jp>
2021-09-03 15:43:51 +0900

    未完成

--- Chapter14/ConsoleApp1/Program.cs
+++ Chapter14/ConsoleApp1/Program.cs
@@ -12,6 +12,13 @@ namespace Section04
 {
     class Program
     {
+        Dictionary<string, int> Area = new Dictionary<string, int>()
+        {
+            {"前橋", 4210},
+            {"みなかみ", 4220},
+            {"宇都宮", 4110},
+            {"水戸", 4010}
+        };
         static void Main(string[] args)
         {
             new Program();
@@ -20,61 +27,36 @@ namespace Section04
         //コンストラクタ
         public Program()
         {
-            //DownloadString();
-            //DownloadFileAsync();
-            //OpenReadSample();
-            var results = GetWeatherReportFromYahoo(4610);
+            Console.WriteLine("yahoo!週間天気予報");
+            Console.WriteLine();//改行
+            Console.WriteLine("地域コードを入力してください");
+            for (int i = 0; i < Area.Count; i++)
+            {
+                Console.WriteLine("{0}:{1}", i + 1, );
+            }
+            Console.WriteLine("");
+            Console.WriteLine("1:前橋");　　　//4210
+            Console.WriteLine("2:みなかみ");　//4220
+            Console.WriteLine("3:宇都宮");　　//4110
+            Console.WriteLine("4:水戸");　　　//4010
+            Console.WriteLine("9:その他(直接入力)");
+            Console.WriteLine();//改行
+
+            Console.Write(">");
+            //文字列として数字を取り込む
+            var selectArea = Console.ReadLine();
+
+            if (true)
+            {
+                int a = int.Parse();
+            }
+            
+            var results = GetWeatherReportFromYahoo(4210);//前橋
             foreach (var s in results)
             {
                 Console.WriteLine(s);
             }
             Console.ReadLine();//入力待ち
-        }
-
-        //リスト14.15
-        public void DownloadString()
-        {
-            var wc = new WebClient();
-            wc.Encoding = Encoding.UTF8;
-            var html = wc.DownloadString("https://yahoo.co.jp");
-            Console.WriteLine(html);
-        }
-
-        //リスト14.17
-        private void DownloadFileAsync()
-        {
-            var wc = new WebClient();
-            var url = new Uri(@"C:\Users\ka32162\Desktop\ダウンロード.jfif");
-            var filename = @"C:\temp\example.zip";
-            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
-            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
-            wc.DownloadFileAsync(url, filename);//非同期でダウンロード
-            Console.ReadLine();//アプリケーションが終了しないように
-        }
-
-        static void wc_DownloadProgressChanged(object sender,
-                            DownloadProgressChangedEventArgs e)
-        {
-            Console.WriteLine("{0}% {1}/{2}", e.ProgressPercentage,
-                              e.BytesReceived, e.TotalBytesToReceive);
-        }
-
-        static void wc_DownloadFileCompleted(object sender,
-                            System.ComponentModel.AsyncCompletedEventArgs e)
-        {
-            Console.WriteLine("ダウンロード完了");
-        }
-
-        //リスト14.18（ストリームとしてDL）
-        public void OpenReadSample()
-        {
-            var wc = new WebClient();
-            using (var stream = wc.OpenRead(@"https://yahoo.co.jp"))
-            using(var sr = new StreamReader(stream,Encoding.UTF8))
-            {
-                string html = sr.ReadToEnd();
-                Console.WriteLine(html);
-            }
         }
 
         //リスト14.19
