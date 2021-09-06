7d2e25992d7dffbddac7f8bb94907e4acac0101c
健太 新井 <32162@ojs.ac.jp>
2021-09-06 16:00:19 +0900

    RssReader　未完

---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            setRssTitle(tbUrl.Text);
+            Console.ReadLine();
+        }
+        //指定したURL先からXMLデータを取得し、title要素を取得し、リストボックスへセットする
+        private void setRssTitle(string url)
+        {
+            var uriString = string.Format(, url);
+
+            var rssXml = new XmlDocument();
+            rssXml.Load(url);
+            XmlElement rssElem = rssXml.DocumentElement;
+            String rssRootNodeTag = rssElem.Name;
+
+            
+        }        
+    }
+}
+
+
+//https://www.yahoo.co.jp/
