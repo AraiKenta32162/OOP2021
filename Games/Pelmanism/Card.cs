f6eab27b03989045a74aef85a6838127c67db174
健太 新井 <32162@ojs.ac.jp>
2021-11-18 11:44:25 +0900

    神経衰弱画面配置完成

---
+++ Games/Pelmanism/Card.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Pelmanism
+{
+    //カードクラス
+    class Card : Button
+    {
+        //カードの横幅、高さ
+        private const int SizeW = 50, SizeH = 70;
+
+        //カードの絵柄
+        public string Picture { get; set; }
+
+        //カードの状態(true:表 false:裏)
+        public bool State { get; set; }
+
+        //カードの表面の色
+        public Color OpenColor { get; } = Color.White;
+
+        //カードの裏面の色
+        public Color CloseColor { get; } = Color.LightBlue;
+
+        public Card(string picture)
+        {
+            Picture = picture;
+            State = false;
+            Size = new Size(SizeW, SizeH);
+            BackColor = CloseColor;
+            Font = new Font("MS UI Gothic", 14, FontStyle.Bold);
+            Enabled = false;
+        }
+
+        //カードをあける
+        public void Open()
+        {
+            State = true;  //表
+            BackColor = OpenColor;
+            Text = Picture;
+            Enabled = false;　//選択不可
+        }
+
+        //カードをとじる
+        public void Close()
+        {
+            State = false;  //表
+            BackColor = CloseColor;
+            Text = "";
+            Enabled = true;　//選択不可
+        }
+
+        //カードをひっくり返す
+        public void Turn()
+        {
+            if (State)
+                Close(); //裏にする
+            else
+                Open();　//表にする
+        }
+    }
+}
---
+++ Games/Pelmanism/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Pelmanism
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSec = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelGuidance = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelSec
+            // 
+            this.labelSec.AutoSize = true;
+            this.labelSec.Location = new System.Drawing.Point(12, 9);
+            this.labelSec.Name = "labelSec";
+            this.labelSec.Size = new System.Drawing.Size(47, 12);
+            this.labelSec.TabIndex = 0;
+            this.labelSec.Text = "0秒経過";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(190, 276);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "スタート";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // labelGuidance
+            // 
+            this.labelGuidance.AutoSize = true;
+            this.labelGuidance.ForeColor = System.Drawing.Color.Red;
+            this.labelGuidance.Location = new System.Drawing.Point(78, 9);
+            this.labelGuidance.Name = "labelGuidance";
+            this.labelGuidance.Size = new System.Drawing.Size(35, 12);
+            this.labelGuidance.TabIndex = 2;
+            this.labelGuidance.Text = "label1";
+            // 
+            // FormGame
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(459, 311);
+            this.Controls.Add(this.labelGuidance);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelSec);
+            this.Name = "FormGame";
+            this.Text = "神経衰弱";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSec;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelGuidance;
+    }
+}
+
