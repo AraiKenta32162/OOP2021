2e88d0e2d35dd3777a5eefbebea935f4cf2bc08d
健太 新井 <32162@ojs.ac.jp>
2021-11-22 15:09:13 +0900

    修正

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -20,21 +20,80 @@ namespace NumberGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random rand = new Random();
+        private int ans;
+        private const int Rows = 5;
+        private const int Columns = 5;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void tbMsg_TextChanged(object sender, TextChangedEventArgs e)
+        //ボタン配置
+        private void SetNumberButton()
         {
-            if (true)
+            List<Button> buttons = new List<Button>();
+            //行
+            for (int i = 0; i < Rows; i++)
             {
-                MessageBox.Show(string.Format("入力された文字は{0}です。", bt1.Text));
+                gridPanel.RowDefinitions.Add(new RowDefinition());
+
+
+            }
+            //列
+            for (int j = 0; j < Columns; j++)
+            {
+                gridPanel.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    var button = new Button();
+                    button.Content = (j + 1) + i * Rows;
+                    button.Click += Button_Click;
+                    button.Height = Main.Height / Rows;
+                    button.Width = Main.Width / Columns;
+                    button.FontSize = 30;
+                    Grid.SetRow(button, i);//行
+                    Grid.SetColumn(button, j);//列
+                    buttons.Add(button);
+                }
+            }
+            buttons.ForEach(button => gridPanel.Children.Add(button));
+            Main.Height += ansText.Height + 50;
+        }
+        //ボタンがクリックされた時のイベントハンドラー
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (ans < int.Parse(((Button)sender).Content.ToString()))
+            {
+                ((Button)sender).Background = Brushes.Gray;
+                ansText.Text = "小さい数です";
+            }
+            else if (ans > int.Parse(((Button)sender).Content.ToString()))
+            {
+                ((Button)sender).Background = Brushes.Gray;
+                ansText.Text = "大きい数です";
             }
             else
             {
-
+                ((Button)sender).Background = Brushes.LawnGreen;
+                ansText.Text = "正解です";
             }
+        }
+        //答えを生成
+        private void SetAnswerNumber()
+        {
+            ans = rand.Next(Rows * Columns) + 1;
+        }
+        private void Win_Load(object sender, RoutedEventArgs e)
+        {
+            SetAnswerNumber();
+            SetNumberButton();
         }
     }
 }
