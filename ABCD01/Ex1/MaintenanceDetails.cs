242126ead56a1392bd6dcdfba7650a1893241f63
健太 新井 <32162@ojs.ac.jp>
2022-01-20 15:54:38 +0900

    卒業制作03-1

--- ABCD01/Ex1/MaintenanceDetails.cs
+++ ABCD01/Ex1/MaintenanceDetails.cs
@@ -23,34 +23,36 @@ namespace Ex1
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
-                if (ofdFileOpen.ShowDialog() == DialogResult.OK)
-                {
-                    //バイナリー形式で逆シリアル化
-                    var bf = new BinaryFormatter();
-                    using (FileStream fs = File.Open(ofdFileOpen.FileName, FileMode.Open, FileAccess.Read))
-                    {
-                        listCarReport = (BindingList<CarReport>)bf.Deserialize(fs);
-                        dgvRegistData.DataSource = null;
-                        dgvRegistData.DataSource = listCarReport;
-                    }
 
-                }
+            //    if (ofdFileOpen.ShowDialog() == DialogResult.OK)
+            //    {
+            //        //バイナリー形式で逆シリアル化
+            //        var bf = new BinaryFormatter();
+            //        using (FileStream fs = File.Open(ofdFileOpen.FileName, FileMode.Open, FileAccess.Read))
+            //        {
+            //            listCarReport = (BindingList<CarReport>)bf.Deserialize(fs);
+            //            dgvRegistData.DataSource = null;
+            //            dgvRegistData.DataSource = listCarReport;
+            //        }
 
-            }catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
+            //    }
 
-            }
-            foreach (var item in listCarReport)
-            {
-                setCbAuther(item.Auther);
-                setCbCarName(item.CarName);
-            }
+            //}catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+
+            //}
+            //foreach (var item in listCarReport)
+            //{
+            //    setCbAuther(item.Auther);
+            //    setCbCarName(item.CarName);
+            //}
+
+            /***********************************/
             //for (int i=0;i < dgvRegistData.RowCount; i++) {
             //    setCbAuther(dgvRegistData.Rows[i].Cells[1].ToString());
             //    setCbCarName(dgvRegistData.Rows[i].Cells[3].ToString());
             //}
-        
+        }
     }
 }
