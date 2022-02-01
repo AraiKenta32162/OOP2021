f89fb0faa1d0a19ce669ac7fd4f5478765d80299
健太 新井 <32162@ojs.ac.jp>
2022-02-01 14:05:36 +0900

    a

--- ABCD01/Ex1/Tooling.cs
+++ ABCD01/Ex1/Tooling.cs
@@ -104,8 +104,8 @@ namespace Ex1
             //if (TouringReport.dgvTouringData.CurrentRow == null) return;
             if (infosys202119Touring.DataSetName != null) return;
 
-            infosys202119Touring.DataSetName = dtpDate.CustomFormat;//日付
             //TouringReport.dgvTouringData.CurrentRow.Cells[2].Value = tbAuthor.Text;
+            infosys202119Touring.DataSetName.Cells[1].Value = dtpDate.CustomFormat;//日付
             infosys202119Touring.DataSetName = tbAuthor.Text;
             infosys202119Touring.DataSetName = Namecb.Text;
             infosys202119Touring.DataSetName = Distancetb.Text;
@@ -115,9 +115,9 @@ namespace Ex1
             infosys202119Touring.DataSetName = (string)ImageToByteArray(pbPicture.Image);
 
             this.Validate();
-            TouringReport.touringTableAdapter.Dispose();
+            //TouringReport.touringTableAdapter.Dispose();
             TouringReport.tableAdapterManager.UpdateAll(this.infosys202119Touring);
-
+            
             MessageBox.Show("保存しました。");
         }
 
